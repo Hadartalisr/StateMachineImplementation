a1ab1a65ef2fe90e581eb1eeeea723589ad41e00
HadarTal <hadar.tal.isr@gmail.com>
2021-07-17 15:30:10 +0300

    WIP

---
+++ src/main/java/code/StateMachineImplementation/StateMachineImplementationApplication.java
@@ -0,0 +1,13 @@
+package code.StateMachineImplementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StateMachineImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StateMachineImplementationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/code/StateMachineImplementation/StateMachineImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package code.StateMachineImplementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StateMachineImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
