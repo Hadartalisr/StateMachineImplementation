9bbcc6d1b2a67dd574b0f7eb7185f180079f5850
HadarTal <hadar.tal.isr@gmail.com>
2021-07-18 00:01:51 +0300

    created post method for the process in the controller

--- src/main/java/controllers/MachineController.java
+++ src/main/java/controllers/MachineController.java
@@ -7,10 +7,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import machine.models.Event;
+import machine.models.MachineProcessResponse;
 import machine.models.State;
+import machine.models.Status;
 import services.MachineService;
 
 @RestController
@@ -35,23 +43,40 @@ public class MachineController {
 		HttpStatus httpStatus = HttpStatus.OK;
 		Boolean returnBool = true;
 		if (isRunning.equalsIgnoreCase("true") || isRunning.equalsIgnoreCase("false")) {
-		    this.machineService.setIsRunning(Boolean.valueOf(isRunning));
+			this.machineService.setIsRunning(Boolean.valueOf(isRunning));
 		} else {
 			returnBool = false;
 			httpStatus = HttpStatus.BAD_REQUEST;
 		}
 		return new ResponseEntity<Boolean>(returnBool, httpStatus);
 	}
-	
+
 	@GetMapping("/currentState")
 	public ResponseEntity<State> getCurrentState() {
 		State state = this.machineService.getCurrentState();
 		return new ResponseEntity<State>(state, HttpStatus.OK);
 	}
 
-//	public MachineProcessResponse process(Event<?> event) {
-//		
-//	}
-//	
+	@PostMapping("/process")
+	public ResponseEntity<MachineProcessResponse> process(@RequestBody Object eventObject) {
+		JsonObject json = JsonParser.parseString(eventObject.toString()).getAsJsonObject();
+		String type = json.get("type").getAsString();
+		Class<?> clazz = null;
+		Object object = null;
+		try {
+			clazz = Class.forName(String.format("models.%s", type));
+			object = new Gson().fromJson(json, clazz);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			// TODO if the JSON do not contain the type field
+		}
+		Event<?> event = new Event<>(object);   
+		HttpStatus httpStatus = HttpStatus.OK;
+		MachineProcessResponse processResponse = this.machineService.process(event);
+		if(!processResponse.getStatus().equals(Status.OK)) {
+			httpStatus = HttpStatus.BAD_REQUEST;
+		}
+		return new ResponseEntity<MachineProcessResponse>(processResponse, httpStatus);
+	}
 
 }
--- src/main/java/models/Click.java
+++ src/main/java/models/Click.java
@@ -1,5 +1,6 @@
 package models;
 
-public interface Click {
-
+public class Click {
+	
+	
 }
